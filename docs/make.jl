58c065eb6c3ad12742ee4ebc0649912363af55ad
Hiroharu Sugawara <hsugawa@gmail.com>
2021-09-13 17:03:56 +0900

    add docs/src

---
+++ docs/make.jl
@@ -0,0 +1,47 @@
+using Documenter
+
+# using memoProgClass2021w
+module memoProgClass2021w
+end
+
+using Documenter
+
+makedocs(;
+    modules=[memoProgClass2021w],
+    authors="Hiroharu Sugawara <hsugawa@gmail.com>",
+    repo="https://github.com/hsugawa8651/memoProgClass2021w.jl/blob/{commit}{path}#L{line}",
+    sitename="memoProgClass2021w.jl",
+    format=Documenter.HTML(;
+        prettyurls=get(ENV, "CI", "false") == "true",
+        canonical="https://hsugawa8651.github.io/memoProgClass2021w.jl",
+        assets=String[],
+    ),
+	pages= [
+	"Home" => "index.md",
+		"LICENSE.md",
+		"LICENSEja.md",
+		"ch00.md",
+		"ch01.md",
+		"ch02.md",
+		"ch03.md",
+		"ch04.md",
+		"ch05.md",
+		"ch06.md",
+		"ch07.md",
+		"ch08.md",
+		"ch09.md",
+		"ch10.md",
+		"ch11.md",
+		"ch12.md",
+		"ch13.md",
+		# "porting.md",
+		"index.md"
+	]
+)
+
+#=
+deploydocs(;
+    repo="github.com/hsugawa8651/memoProgClass2021w.jl",
+)
+=#
+
---
+++ docs/src/index.md
@@ -0,0 +1,48 @@
+```@meta
+CurrentModule = memoProgClass2021w
+```
+# 2021年度版
+
+この内容は，2020年度後期の授業（2021年10月から2022年1月）向けに公開するものです．
+- 実行結果は，Julia 1.6.2 （→ [Julia v1.6 Release Notes](https://docs.julialang.org/en/v1/NEWS/#Julia-v1.6-Release-Notes) ）により出力されたものです．
+
+## Preface
+
+```@contents
+Pages=[
+	"ch00.md",
+	"LICENSE.md",
+	"LICENSEja.md"
+]
+```
+
+## Main
+
+```@contents
+Pages=[
+	"ch01.md",
+	"ch02.md",
+	"ch03.md",
+	"ch04.md",
+	"ch05.md",
+	"ch06.md",
+	"ch06.md",
+	"ch07.md",
+	"ch08.md",
+	"ch08.md",
+	"ch09.md",
+	"ch10.md",
+	"ch11.md",
+	"ch12.md",
+	"ch13.md"
+]
+```
+
+## Appendices
+
+```@contents
+Pages=[
+	"ch0a.md",
+	"ch0b.md"
+]
+```
